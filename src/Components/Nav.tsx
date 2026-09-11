aa3dc7bd94a2b6578b8c704151c6f41b81f23db0
meherinamukta03 <meherinamukta03@gmail.com>
2026-09-11 14:40:11 +0600

    my Vavbar Commit

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,92 @@
+import  { useState } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
+
+
+const Nav=() => {
+
+    const [isOpen,setIsOpen]=useState(false)
+
+
+    return (
+       <nav className='sticky top-0 bg-white shadow-nd'>
+            <div className='flex  items-center justify-between px-5 py-4m '>
+
+                  
+
+        <button
+          className="md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <RxHamburgerMenu size={25} />
+        </button>
+
+        {/* dextop */}
+
+                <div className='text-2xl'> 
+                    
+                     <img src="/assets/logo-text.png"alt="" />
+                     
+                  </div>
+                <div>
+               <ul className=' hidden md:flex items-center gap-6'>
+
+
+  <li className='text-pink-600'>Home</li>
+  <li  className='color: #374151' >Technologies</li>
+  <li className='color: #374151'>Projects </li>
+  <li className='color: #374151'> About </li>
+  <li className='color: #374151'>Center</li>
+               </ul>
+
+               </div>
+
+
+{/* sign in ,up */}
+
+
+
+<div className='flex items-center gap-3'>
+
+<button className='text-sm'>Sign In</button>
+<button className='btn-primary bg-pink-600 text-white px-6 py-2 rounded-full'>Sign Up</button>
+</div>
+                
+  
+
+ 
+  
+</div>
+{isOpen&& (
+
+    <div className="md:hidden px-5 pb-5">
+          <ul className="flex flex-col gap-4">
+            <li className="text-pink-600">Home</li>
+            <li className="text-gray-700">Technologies</li>
+            <li className="text-gray-700">Projects</li>
+            <li className="text-gray-700">About</li>
+            <li className="text-gray-700">Contact</li>
+          </ul>
+        </div>
+
+  
+
+
+
+
+
+)}
+
+ 
+
+
+        
+          
+         
+
+
+
+       </nav>
+    );
+};
+
+export default Nav;
