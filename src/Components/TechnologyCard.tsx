270b60a957dad8e72feb5fa63f9dbe0e5132a226
meherinamukta03 <meherinamukta03@gmail.com>
2026-09-12 13:02:47 +0600

    commit for technology

--- src/Components/TechnologyCard.tsx
+++ src/Components/TechnologyCard.tsx
@@ -1,20 +1,30 @@
 
 
+import { use } from 'react';
 import type IdataType from '../Types/dataType';
+import Card from './Card';
 
-const Technology= ({DataPromise} :IdataType)=> {
+type TechnologyProps = {
+  DataPromise: Promise<IdataType[]>;
+};
+
+
+const Technology= ({DataPromise} :TechnologyProps)=> {
+
+
+    const CardInfo=use(DataPromise)
     return (
         <div >
         <div>
       <h1 className="text-5xl font-bold mx-15">
       Explore The<span className=' text-pink-600'> Technologies</span>
       </h1>
-        <p className='text-gray-500 mx-15 mt-3'>Pick one technology per category to build your ideal stack</p>
+        <p className='text-gray-500 mx-15 mt-3 font-'>Pick one technology per category to build your ideal stack</p>
 
         </div>
 
-
-
+          
+      <Card CardInfo={CardInfo}></Card>
 
 
 
